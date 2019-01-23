c63d8b6572ae8fa7142380c34b4ca4a6a6ba6762
venky0195 <ven.venky08@gmail.com>
2019-01-23 19:16:22 +0530

    First commit

--- Functional/stringReplace.js
+++ Functional/stringReplace.js
@@ -1,3 +1,10 @@
+/*
+ *  @Purpose        : User Input and Replace String Template “Hello <<UserName>>, How are you?”
+ *  @file           : stringReplace.js
+ *  @overview       : stringReplace takes the userinput and print it with some sentence.
+ *  @author         : VENKATESH G.
+ *  @version        : 1.0.
+ *  */
 
 var Access = require('../Utility/utility');   
 /*
--- Utility/utility.js
+++ Utility/utility.js
@@ -1,17 +1,23 @@
 module.exports = {
-    stringReplace(username) {
-
-        if (username.length > 3) 
+    //Functional Programs
+     /*
+     * @Purpose     : Ensuring username with minimum 3 characters and only alphabets,
+                      replacing <<UserName>> with userinput and print the string.
+     * @description : Declaring the function and passing the userinput as argument.
+     * @function    : stringReplace function takes the userinput and replaces <<UserName>>.
+     */
+     stringReplace(username) {
+    
+        if (username.length>=3 && isNaN(username))
         {
             var input = 'Hello <<UserName>>, How are you?';
             var output = input.replace(/<<UserName>>/g, username);
             //searches for <<UserName>> in the program and replaces it with the input
             console.log(output);
-
         }
         else 
         {
-            console.log('Please enter characters more than 3');
+            console.log('Please enter characters more than 3, only alphabets');
         }
 
     },
